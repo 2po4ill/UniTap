00980c4966b30e896fd790e22de9404877f075ef
2po4ill <ydevlicharov@bk.ru>
2025-07-21 15:00:17 +0300

    fix(deploy): switch to HashRouter for GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './app/App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
